55d600c794f3a82dbda3298397311906c21d9de7
Koen Vlaswinkel <koen@vlaswinkel.info>
2019-02-13 13:14:22 +0100

    Add fields from ESMR 5.0 specification

--- smartmeter/p1.go
+++ smartmeter/p1.go
@@ -3,6 +3,11 @@ package smartmeter
 import "time"
 
 type P1Packet struct {
+	// DSMRVersion is the version information for P1 output (1-3:0.2.8)
+	DSMRVersion string
+	// Timestamp is the date-time stamp of the P1 message (0-0:1.0.0)
+	Timestamp time.Time
+
 	Electricity Electricity
 	Gas         Gas
 	Message     Message
@@ -30,6 +35,17 @@ type Electricity struct {
 	CurrentConsumed float64
 	// CurrentProduced contains the actual electricity power produced in kW (1-0:2.7.0)
 	CurrentProduced float64
+
+	// NumberOfPowerFailures contains the number of power failures in any phase (0-0:96.7.21)
+	NumberOfPowerFailures int
+	// NumberOfLongPowerFailures contains the number of long power failures in any phase (0-0:96.7.9)
+	NumberOfLongPowerFailures int
+
+	// Phases contains the data of phases, currently exactly three phases.
+	Phases []Phase
+
+	// PowerFailureEventLog contains long power failures (1-0:99.97.0)
+	PowerFailureEventLog []PowerFailure
 }
 
 type Tariff struct {
@@ -37,6 +53,29 @@ type Tariff struct {
 	Consumed float64
 	// Produced is the electricity delivered by client during this tariff in kWh (1-0:2.8.1/1-0:2.8.2)
 	Produced float64
+}
+
+type Phase struct {
+	// NumberOfVoltageSage contains the number of voltage sags in this phase (1-0:32.32.0/1-0:52.32.0/1-0:72.32.0)
+	NumberOfVoltageSags int
+	// NumberOfVoltageSwells contains the number of voltage swells in this phase (1-0:32.36.0/1-0:52.36.0/1-0:72.36.0)
+	NumberOfVoltageSwells int
+
+	// InstantaneousVoltage contains the instantaneous voltage in this phase in V (1-0:32.7.0/1-0:52.7.0/1-0:72.7.0)
+	InstantaneousVoltage float64
+	// InstantaneousCurrent contains the instantaneous current in this phase in A (1-0:31.7.0/1-0:51.7.0/1-0:71.7.0)
+	InstantaneousCurrent float64
+	// InstantaneousActivePositivePower contains the instantaneous active power (+P) in this phase in kW (1-0:21.7.0/1-0:41.7.0/1-0:61.7.0)
+	InstantaneousActivePositivePower float64
+	// InstantaneousActivePositivePower contains the instantaneous active power (-P) in this phase in kW (1-0:22.7.0/1-0:42.7.0/1-0:62.7.0)
+	InstantaneousActiveNegativePower float64
+}
+
+type PowerFailure struct {
+	// Timestamp contains the timestamp of the end of the power failure
+	Timestamp time.Time
+	// Duration contains the length of the power failure
+	Duration time.Duration
 }
 
 type Message struct {
--- smartmeter/smartmeter.go
+++ smartmeter/smartmeter.go
@@ -63,6 +63,10 @@ func (sm *SmartMeter) Read() (*P1Packet, error) {
 
 func (sm *SmartMeter) parsePacket(datagram [][]byte) (*P1Packet, error) {
 	p := &P1Packet{
+		Timestamp: time.Now(),
+		Electricity: Electricity{
+			Phases: make([]Phase, 3),
+		},
 		Raw: datagram,
 	}
 	var err error
@@ -77,6 +81,13 @@ func (sm *SmartMeter) parsePacket(datagram [][]byte) (*P1Packet, error) {
 		data := string(line[dataStart+1 : dataEnd])
 
 		switch identifier {
+		case "1-3:0.2.8":
+			p.DSMRVersion = data
+		case "0-0:1.0.0":
+			p.Timestamp, err = time.ParseInLocation(dateFormat, data[:len(data)-1], time.Local)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as timestamp: %v", data, err)
+			}
 		case "0-0:96.1.1":
 			p.Electricity.EquipmentID = data
 		case "0-0:96.14.0":
@@ -148,6 +159,218 @@ func (sm *SmartMeter) parsePacket(datagram [][]byte) (*P1Packet, error) {
 			p.Electricity.CurrentProduced, err = strconv.ParseFloat(data, 64)
 			if err != nil {
 				return nil, fmt.Errorf("failed to parse %v as electricity usage: %v", data, err)
+			}
+		case "0-0:96.7.21":
+			p.Electricity.NumberOfPowerFailures, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of power failures: %v", data, err)
+			}
+		case "0-0:96.7.9":
+			p.Electricity.NumberOfLongPowerFailures, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of long power failures: %v", data, err)
+			}
+		case "1-0:32.32.0":
+			p.Electricity.Phases[0].NumberOfVoltageSags, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of voltage sags in phase L1: %v", data, err)
+			}
+		case "1-0:52.32.0":
+			p.Electricity.Phases[1].NumberOfVoltageSags, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of voltage sags in phase L2: %v", data, err)
+			}
+		case "1-0:72.32.0":
+			p.Electricity.Phases[2].NumberOfVoltageSags, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of voltage sags in phase L3: %v", data, err)
+			}
+		case "1-0:32.36.0":
+			p.Electricity.Phases[0].NumberOfVoltageSwells, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of voltage swells in phase L1: %v", data, err)
+			}
+		case "1-0:52.36.0":
+			p.Electricity.Phases[1].NumberOfVoltageSwells, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of voltage swells in phase L2: %v", data, err)
+			}
+		case "1-0:72.36.0":
+			p.Electricity.Phases[2].NumberOfVoltageSwells, err = strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of voltage swells in phase L3: %v", data, err)
+			}
+		case "1-0:32.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "V" {
+				return nil, fmt.Errorf("invalid unit for instantaneous voltage in phase L1: %v", unit)
+			}
+			p.Electricity.Phases[0].InstantaneousVoltage, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous voltage in phase L1: %v", data, err)
+			}
+		case "1-0:52.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "V" {
+				return nil, fmt.Errorf("invalid unit for instantaneous voltage in phase L2: %v", unit)
+			}
+			p.Electricity.Phases[1].InstantaneousVoltage, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous voltage in phase L2: %v", data, err)
+			}
+		case "1-0:72.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "V" {
+				return nil, fmt.Errorf("invalid unit for instantaneous voltage in phase L3: %v", unit)
+			}
+			p.Electricity.Phases[2].InstantaneousVoltage, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous voltage in phase L3: %v", data, err)
+			}
+		case "1-0:31.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "A" {
+				return nil, fmt.Errorf("invalid unit for instantaneous current in phase L1: %v", unit)
+			}
+			p.Electricity.Phases[0].InstantaneousCurrent, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous current in phase L1: %v", data, err)
+			}
+		case "1-0:51.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "A" {
+				return nil, fmt.Errorf("invalid unit for instantaneous current in phase L2: %v", unit)
+			}
+			p.Electricity.Phases[1].InstantaneousCurrent, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous current in phase L2: %v", data, err)
+			}
+		case "1-0:71.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "A" {
+				return nil, fmt.Errorf("invalid unit for instantaneous current in phase L3: %v", unit)
+			}
+			p.Electricity.Phases[2].InstantaneousCurrent, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous current in phase L3: %v", data, err)
+			}
+		case "1-0:21.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "kW" {
+				return nil, fmt.Errorf("invalid unit for instantaneous active power P+ in phase L1: %v", unit)
+			}
+			p.Electricity.Phases[0].InstantaneousActivePositivePower, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous active power P+ in phase L1: %v", data, err)
+			}
+		case "1-0:41.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "kW" {
+				return nil, fmt.Errorf("invalid unit for instantaneous active power P+ in phase L2: %v", unit)
+			}
+			p.Electricity.Phases[1].InstantaneousActivePositivePower, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous active power P+ in phase L2: %v", data, err)
+			}
+		case "1-0:61.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "kW" {
+				return nil, fmt.Errorf("invalid unit for instantaneous active power P+ in phase L3: %v", unit)
+			}
+			p.Electricity.Phases[2].InstantaneousActivePositivePower, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous active power P+ in phase L3: %v", data, err)
+			}
+		case "1-0:22.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "kW" {
+				return nil, fmt.Errorf("invalid unit for instantaneous active power P- in phase L1: %v", unit)
+			}
+			p.Electricity.Phases[0].InstantaneousActiveNegativePower, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous active power P- in phase L1: %v", data, err)
+			}
+		case "1-0:42.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "kW" {
+				return nil, fmt.Errorf("invalid unit for instantaneous active power P- in phase L2: %v", unit)
+			}
+			p.Electricity.Phases[1].InstantaneousActiveNegativePower, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous active power P- in phase L2: %v", data, err)
+			}
+		case "1-0:99.97.0":
+			numberOfPowerFailures, err := strconv.Atoi(data)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as number of power failures: %v", data, err)
+			}
+
+			index := dataEnd + 1
+			lineData := line[index:]
+
+			nextDataStart := bytes.IndexRune(lineData, '(')
+			nextDataEnd := bytes.IndexRune(lineData, ')')
+			if nextDataStart < 0 || nextDataEnd < 0 {
+				continue
+			}
+			data := string(lineData[nextDataStart+1 : nextDataEnd])
+
+			if data != "0-0:96.7.19" {
+				return nil, fmt.Errorf("invalid power failure event log data format %v", data)
+			}
+
+			for i := 0; i < numberOfPowerFailures; i++ {
+				item := PowerFailure{}
+
+				index = nextDataEnd + 1
+				lineData = lineData[index:]
+
+				nextDataStart = bytes.IndexRune(lineData, '(')
+				nextDataEnd = bytes.IndexRune(lineData, ')')
+				if nextDataStart < 0 || nextDataEnd < 0 {
+					continue
+				}
+
+				data := string(lineData[nextDataStart+1 : nextDataEnd])
+
+				item.Timestamp, err = time.ParseInLocation(dateFormat, data[:len(data)-1], time.Local)
+				if err != nil {
+					return nil, fmt.Errorf("failed to parse %v as power failure timestamp: %v", data, err)
+				}
+
+				index = nextDataEnd + 1
+				lineData = lineData[index:]
+
+				nextDataStart = bytes.IndexRune(lineData, '(')
+				nextDataEnd = bytes.IndexRune(lineData, ')')
+				if nextDataStart < 0 || nextDataEnd < 0 {
+					continue
+				}
+
+				data = string(lineData[nextDataStart+1 : nextDataEnd])
+
+				data, unit := sm.getValueAndUnit(data)
+				if unit != "s" {
+					return nil, fmt.Errorf("invalid unit for power failure event log duration: %v", unit)
+				}
+
+				duration, err := strconv.Atoi(data)
+				if err != nil {
+					return nil, fmt.Errorf("failed to parse %v as power failure duration: %v", data, err)
+				}
+
+				item.Duration = time.Duration(duration) * time.Second
+
+				p.Electricity.PowerFailureEventLog = append(p.Electricity.PowerFailureEventLog, item)
+			}
+		case "1-0:62.7.0":
+			data, unit := sm.getValueAndUnit(data)
+			if unit != "kW" {
+				return nil, fmt.Errorf("invalid unit for instantaneous active power P- in phase L3: %v", unit)
+			}
+			p.Electricity.Phases[2].InstantaneousActiveNegativePower, err = strconv.ParseFloat(data, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse %v as instantaneous active power P- in phase L3: %v", data, err)
 			}
 		case "0-1:96.1.0":
 			p.Gas.EquipmentID = data
